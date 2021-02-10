3b1d333d725f9e2ab0e1d906ca9c7c3f18ee93ae
Jasper Strijp <jasperstrijp@gmail.com>
2021-02-10 12:00:43 +0100

    added select dataset and dynamic dataset loader

---
+++ js/model/dataset.js
@@ -0,0 +1,13 @@
+class Dataset{
+    name;
+    url;
+    dataKey;
+    labelKey;
+
+    constructor(name, url, dataKey, labelKey) {
+        this.name = name;
+        this.url = url;
+        this.dataKey = dataKey;
+        this.labelKey = labelKey;
+    }
+}

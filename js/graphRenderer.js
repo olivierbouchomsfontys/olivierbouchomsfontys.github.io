9f41120c91be8abf8653a36cb21bf74f4d6c8cec
Olivier Bouchoms <o.bouchoms@student.fontys.nl>
2021-02-10 14:00:26 +0100

    Add color scheme for graphs

--- js/graphRenderer.js
+++ js/graphRenderer.js
@@ -1,4 +1,9 @@
 class GraphRenderer {
+    colorPrimary = getComputedStyle(document.documentElement).getPropertyValue('--graph-primary');
+    colorSecondary = getComputedStyle(document.documentElement).getPropertyValue('--graph-secondary');
+    colorTertiary = getComputedStyle(document.documentElement).getPropertyValue('--graph-tertiary');
+    colorQuaternary = getComputedStyle(document.documentElement).getPropertyValue('--graph-quaternary');
+
     renderGraphByCsv(url, dataKey, labelKey, element, graphType) {
         const graphRenderer = this;
         d3.csv(url)
@@ -22,7 +27,12 @@ class GraphRenderer {
                 labels: labels,
                 datasets: [
                     {
-                        data: data
+                        data: data,
+                        borderColor: this.getBorderColor(graphType),
+                        backgroundColor: this.getBackgroundColor(data, graphType),
+                        fillColor: this.colorPrimary,
+                        highlightFill: this.colorSecondary,
+                        highlightStroke: this.colorSecondary
                     }
                 ]
             }
@@ -113,5 +123,49 @@ class GraphRenderer {
 
         Plotly.newPlot(element, plotData, layout);
 
+    }
+
+    getBackgroundColor(data, graphType) {
+        switch (graphType) {
+            case 'line':
+                return this.getBackgroundColorLine();
+            default:
+                return this.getBackgroundColorOther(data);
+        }
+    }
+
+    getBackgroundColorLine() {
+        return 'transparent';
+    }
+
+    getBackgroundColorOther(data) {
+        if (data.length > 4) {
+            return Please.make_color({ hue: 50, saturation: 0.55, colors_returned: data.length + 1 });
+        }
+
+        return data.map((row, index) => {
+            if (index % 4 === 0) {
+                return this.colorQuaternary;
+            }
+
+            if (index % 3 === 0) {
+                return this.colorTertiary;
+            }
+
+            if (index % 2 === 0) {
+                return this.colorSecondary;
+            }
+
+            return this.colorPrimary;
+        });
+    }
+
+    getBorderColor(graphType) {
+        switch (graphType) {
+            case 'line':
+                return this.colorPrimary;
+            default:
+                return 'transparent';
+        }
     }
 }

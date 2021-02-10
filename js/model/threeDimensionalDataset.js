37c9ca373b1643f823efcacf6727def9183e93f4
Sander de Boer <sander.de.boer4798@hotmail.com>
2021-02-10 14:07:35 +0100

    3d graph selection works

---
+++ js/model/threeDimensionalDataset.js
@@ -0,0 +1,15 @@
+class ThreeDimensionalDataSet{
+    name;
+    url;
+    xKey;
+    yKey;
+    zKey;
+
+    constructor(name, url, xKey, yKey, zKey) {
+        this.name = name;
+        this.url = url;
+        this.xKey = xKey;
+        this.yKey = yKey;
+        this.zKey = zKey;
+    }
+}

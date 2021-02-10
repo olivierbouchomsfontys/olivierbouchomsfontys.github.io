6beee228adb192fcd876a37535d992f448ccabdd
Yannick Peelen <yannickpeelen@gmail.com>
2021-02-10 22:25:20 +0100

    setup dynamic headers in only one dataset

--- js/model/dataset.js
+++ js/model/dataset.js
@@ -3,6 +3,7 @@ class Dataset{
     url;
     dataKey;
     labelKey;
+    headers;
 
     getDataKey() {
         return this.dataKey;
@@ -12,10 +13,11 @@ class Dataset{
         return this.labelKey;
     }
 
-    constructor(name, url, dataKey, labelKey) {
+    constructor(name, url, dataKey, labelKey, headers) {
         this.name = name;
         this.url = url;
         this.dataKey = dataKey;
         this.labelKey = labelKey;
+        this.headers = headers;
     }
 }
